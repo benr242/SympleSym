b3dc5c2257afafdb2e842db37a5cab46dd453bb1
Benjamin Rose <benr242@new-host-2.home>
2017-04-19 22:32:33 -0400

    stacrted database

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benr242
+ * Date: 4/19/17
+ * Time: 7:56 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}

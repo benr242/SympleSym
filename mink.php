6dc82234eb4b2ebd13a0ed2ad6986c8f395fc9e2
Benjamin Rose <benr242@localhost.localdomain>
2017-07-29 00:20:38 -0400

    css

--- mink.php
+++ mink.php
@@ -16,7 +16,7 @@ echo "Status code: ". $session->getStatusCode() . "\n";
 echo "Current URL: ". $session->getCurrentUrl() . "\n";
 
 $page = $session->getPage();
-//echo "First 75 chars: ".substr($page->getText() , 0, 75) . "\n";
+echo "First 75 chars: ".substr($page->getText() , 0, 75) . "\n";
 
 $header = $page->find('css', '.WikiHeader .WikiNav h2');
 //echo "The wiki nav text is: ".$header->getText()."\n";

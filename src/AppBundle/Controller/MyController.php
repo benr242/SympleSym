04f75a97e7062418ad76c8ce2a49645eabb18cc5
Benjamin Rose <benr242@localhost.localdomain>
2017-08-13 18:35:17 -0400

    repos import

--- src/AppBundle/Controller/MyController.php
+++ src/AppBundle/Controller/MyController.php
@@ -16,7 +16,7 @@ class MyController extends Controller
      */
     public function githutAction(Request $request)
     {
-        return $this->render('githut/index.html.twig', [
+        $templateData = [
             'avatar_url'  => 'https://avatars.githubusercontent.com/u/12968163?v=3',
             'name'        => 'Code Review Videos',
             'login'       => 'codereviewvideos',
@@ -30,9 +30,35 @@ class MyController extends Controller
                 'followers'    => 11,
                 'following'    => 22,
                 'public_repos' => 33,
-            ]
-        ]);
+            ],
+
+            'repo_count' => 100,
+            'most_stars' => 50,
+            'repos' => [
+                [
+                    'url' => 'https://codereviewvideos.com',
+                    'name' => 'Code Review Videos',
+                    'description' => 'some repo description',
+                    'stargazers_count' => '999',
+                ],
+                [
+                    'url' => 'http://bbc.co.uk',
+                    'name' => 'The BBC',
+                    'description' => 'not a real repo',
+                    'stargazers_count' => '666',
+                ],
+                [
+                    'url' => 'http://google.co.uk',
+                    'name' => 'Google',
+                    'description' => 'another fake repo description',
+                    'stargazers_count' => '333',
+                ],
+            ],
+        ];
+
+        return $this->render('githut/index.html.twig', $templateData);
     }
+
 
 
     /**
